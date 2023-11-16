cb73d26973fc33711ee3b439cbd6f2c34171e1b4
SY ALIOU <aliouamadousy94@gmail.com>
2023-11-16 15:08:59 +0100

    ajout du fichier json pour ls stockage des données et modif du code

--- GO Lang/projet1/main.go
+++ GO Lang/projet1/main.go
@@ -1,18 +1,42 @@
+// main.go
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	dictionary "projet1/directory/directories"
 )
 
 func main() {
+	// Charger les données depuis le fichier JSON
+	data, err := ioutil.ReadFile("donnees.json")
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier JSON:", err)
+		return
+	}
 
+	// Définir la structure pour stocker les données du fichier JSON
+	var jsonData struct {
+		Data []map[string]string `json:"data"`
+	}
+
+	// Décoder les données JSON dans la structure
+	err = json.Unmarshal(data, &jsonData)
+	if err != nil {
+		fmt.Println("Erreur lors du décodage JSON:", err)
+		return
+	}
+
+	// Initialiser le dictionnaire avec les données du fichier JSON
 	myDictionary := make(dictionary.Dictionary)
+	for _, item := range jsonData.Data {
+		for word, definition := range item {
+			myDictionary.Add(word, definition)
+		}
+	}
 
-	myDictionary.Add("go", "language de programmation")
-	myDictionary.Add("mathématique", "science exacte")
-	myDictionary.Add("mma", "sport de combat")
-
+	// Utiliser le dictionnaire
 	definition, err := myDictionary.Get("go")
 	if err != nil {
 		fmt.Println(err)
